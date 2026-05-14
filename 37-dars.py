6bfa5c53121033335331c8075cc03a498d144015
Qadamov Xasanboy <qadamovxasanboy.01@gmail.com>
2026-05-14 17:38:44 +0500

    Add files via upload

---
+++ 37-dars.py
@@ -0,0 +1,17 @@
+person={
+    'firstname':'Rahmatjon',
+    'lastname':'Komiljonov',
+    'age':15,
+    'school':'bagat PS'
+}
+print(person)
+print(type(person))
+print(person['firstname'])
+print(f"{person['firstname']}ning yoshi {person['age']}")
+person['age']=16
+
+thisdict={
+    'brand': 'Ford',
+    'electric': False,
+    'year': 1964
+}
